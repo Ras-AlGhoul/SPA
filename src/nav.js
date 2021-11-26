c123e28e050c33425e205acfdc43c0dbc50c7669
AndelijaKaluderovic <andeka011@gmail.com>
2021-11-26 21:22:59 +0100

    formatting

--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,6 @@
 const nav = document.createElement('nav');
 const navImg = document.createElement('img');
+
 navImg.src = 'https://i.ibb.co/qR3Zyt0/1.png';
 navImg.setAttribute('class', 'navImg');
 nav.append(navImg);
